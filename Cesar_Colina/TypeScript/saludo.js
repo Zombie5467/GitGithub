b89d602d9533f82b2f1af98d05467ae7980c4de8
Addel <sigmamediacode@gmail.com>
2023-03-09 19:31:33 -0400

    comment on file saludo.js

---
+++ Cesar_Colina/TypeScript/saludo.js
@@ -0,0 +1,7 @@
+// function sayHello(name) {
+//     return "Hello " + name;
+// }
+// var myName = "Zombie#5467";
+// var result = sayHello(myName);
+
+// console.log(result);
---
+++ Cesar_Colina/TypeScript/saludo.ts
@@ -0,0 +1,9 @@
+function sayHello (name) {
+    return "Hello " + name
+}
+
+let myName = "Zombie#5467"
+let result = sayHello(myName)
+
+
+// Now the test is to make an function that take a name given by the user and print an massage, do this with the js and py files, also with this one, tsc this to js.
