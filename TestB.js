d626155ebfff564138c9f01370c0c0294fc51883
Addel <sigmamediacode@gmail.com>
2023-03-09 21:40:47 -0400

    adding new files and modifications

---
+++ TestB.js
@@ -0,0 +1,10 @@
+function helloClient() {
+    const clientMovie = prompt("What's the name of the movie?");
+    const clientYear = prompt("What's the release year?");
+    const clientDirector = prompt("Who's the director?");
+    
+}
+
+console.log(`My favorite movie is ${clientMovie}, released ${clientYear} directed ${clientDirector}`)
+
+// Fix this function and do it with py and ts
